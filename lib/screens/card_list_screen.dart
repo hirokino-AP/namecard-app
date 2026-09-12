fc465e20ef3cf6463c8b01a6b2cf0c51f2f53ae1
hirokino-AP <hiro0001.mobile@gmail.com>
2026-09-12 12:37:44 +0000

    fix: FilePicker.platformに戻す

--- lib/screens/card_list_screen.dart
+++ lib/screens/card_list_screen.dart
@@ -62,7 +62,7 @@ class _CardListScreenState extends State<CardListScreen> {
 
 
   Future<void> _importDb() async {
-    final result = await FilePicker.instance.pickFiles(
+    final result = await FilePicker.platform.pickFiles(
       type: FileType.any,
     );
     if (result == null || result.files.isEmpty) return;
