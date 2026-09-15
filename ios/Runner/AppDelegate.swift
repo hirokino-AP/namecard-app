01f76d5349030ed39ef37de1c81fe6433d5b1ae6
hirokino-AP <hiro0001.mobile@gmail.com>
2026-09-15 10:45:08 +0000

    fix: AppDelegate FlutterImplicitEngineDelegate復活・クラッシュ修正

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,35 +3,41 @@ import UIKit
 import UniformTypeIdentifiers
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, UIDocumentPickerDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate, UIDocumentPickerDelegate {
   private var documentPickerResult: FlutterResult?
+  private var flutterChannel: FlutterMethodChannel?
 
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 
-    let controller = window?.rootViewController as! FlutterViewController
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+
     let channel = FlutterMethodChannel(
       name: "com.hirokino.namecardapp/document_picker",
-      binaryMessenger: controller.binaryMessenger
+      binaryMessenger: engineBridge.binaryMessenger
     )
+    self.flutterChannel = channel
 
     channel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
+      guard let self = self else { return }
       if call.method == "pickDatabase" {
-        self?.documentPickerResult = result
+        self.documentPickerResult = result
         let types = [UTType.data]
         let picker = UIDocumentPickerViewController(forOpeningContentTypes: types)
         picker.delegate = self
         picker.allowsMultipleSelection = false
-        controller.present(picker, animated: true)
+        if let vc = UIApplication.shared.windows.first?.rootViewController {
+          vc.present(picker, animated: true)
+        }
       } else {
         result(FlutterMethodNotImplemented)
       }
     }
-
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
@@ -39,7 +45,7 @@ import UniformTypeIdentifiers
       documentPickerResult?(FlutterError(code: "NO_FILE", message: "ファイルが選択されませんでした", details: nil))
       return
     }
-    url.startAccessingSecurityScopedResource()
+    _ = url.startAccessingSecurityScopedResource()
     let docsDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
     let destURL = docsDir.appendingPathComponent("import_picked.db")
     try? FileManager.default.removeItem(at: destURL)
